1a77084c0c59858c64301c7933a408fd17efc8cb
Levi Li <Levi.Li@tdsynnex.com>
2024-02-20 12:43:57 +0800

    groovy debug

--- src/main/java/com/levil/mongo/groovy/Test2.groovy
+++ src/main/java/com/levil/mongo/groovy/Test2.groovy
@@ -3,4 +3,6 @@ package com.levil.mongo.groovy
 import com.github.managetech.utils.SpringContext
 import com.levil.mongo.api.Api
 def api22 = SpringContext.getBean(Api.class).api22()
-return api22
+binding.setVariable("api22", api22)
+String currentDir = System.getProperty("user.dir")
+return currentDir
